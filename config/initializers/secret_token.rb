f028fec7562dd5a68b07840024406f54a842ea5d
Bob Vonderau <r.vonderau@gmail.com>
2014-04-30 19:41:22 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pampercy::Application.config.secret_key_base = '03dfe9af07da48d5d063e354763e18fc740f0f234e883677a9b89dd9f911c02ef376d4ee165a53f854f93173a58676041034ea2fdf57d85a712b70940f84b262'
