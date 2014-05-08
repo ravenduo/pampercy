7522c8b3871b3c286d18d03426669efa9b120a89
Bob Vonderau <r.vonderau@gmail.com>
2014-05-08 10:39:54 -0500

    Migration for adding 'category'to product model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140508152951) do
+ActiveRecord::Schema.define(version: 20140508153834) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140508152951) do
     t.integer  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "category"
   end
 
   create_table "users", force: true do |t|
