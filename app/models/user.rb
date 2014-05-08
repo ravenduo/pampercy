74c3ea0ac947bb8cf4cb1ccf880b50dfa81d759c
Bob Vonderau <r.vonderau@gmail.com>
2014-05-08 10:26:22 -0500

    Adding relationship to model, adding validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+	belongs_to :company
+	has_many :order
+	has_many :billing_info
+
+
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :email, presence: true
+	validates :password, presence: true
+	validates :phone, presence: true
+
 end
