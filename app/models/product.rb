3e8c92932659eb5ae6c381d159394f0188e6d740
Bob Vonderau <r.vonderau@gmail.com>
2014-05-08 11:02:27 -0500

    Updating Products model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+	validates :name, presence: true
+	validates :description, presence: true
+	validates :category, presence: true
+	validates :weight, presence: true
+	validates :sku, presence: true
+	validates :price, presence: true
+
+	belongs_to :order_products
 end
