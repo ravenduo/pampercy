6757908b2959a193d69d8774700bcd7ec5647257
Bob Vonderau <r.vonderau@gmail.com>
2014-05-08 11:25:32 -0500

    Adding validation and relationships to Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,13 @@
 class Order < ActiveRecord::Base
+	validates :user_id, presence: true
+	validates :shipping_location_id, presence: true
+	validates :shipping_method_id, presence: true
+	validates :order_status_id, presence: true
+	validates :order_price, presence: true
+
+	has_many :order_items
+	has_many :prodcts, :through => :order_items
+	has_one :shipping_locations
+	has_one :shipping_methods
+	has_many :order_statuses
 end
